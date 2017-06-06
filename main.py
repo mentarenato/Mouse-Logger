a5fb0ab008296efdbeef737b6998caf5ebc198b6
rmenta <rmenta@student.ethz.ch>
2017-06-06 13:51:06 +0200

    Improved behaviour, new vector format
    Program does not terminate when switching windows but only stops logging
    the mouse
    
    Vector graphic now uses one single polygon instead of hundreds of lines,
    reduces size by an order of magnitude

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 import ctypes
 import os
 import svgCreator
+import time
 
 from datetime import datetime
 from win32gui import GetWindowText, GetForegroundWindow
@@ -12,9 +13,9 @@ from svgCreator import render_svg
 
 #settings for recording
 title = 'League of Legends (TM) Client'
-debug = True
+debug = False
 if debug:
-    title = 'Python 3.6.1'
+    title = 'Chrome'
 
 #settings for display
 SCALING_FACTOR = 0.5
@@ -25,13 +26,36 @@ screenWidth = int(monitorWidth * SCALING_FACTOR)
 screenHeight = int(monitorHeight * SCALING_FACTOR)
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (-screenWidth, 35)
 
+#system stuff
+activeWindows = []
+EnumWindows = ctypes.windll.user32.EnumWindows
+EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
+GetWindowText = ctypes.windll.user32.GetWindowTextW
+GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+
 
 #colors used in the display
 COLOR_BACKGROUND = (0, 0, 0)
-COLOR_LINE = (255, 255, 255, 0.1)
+COLOR_LINE = (100, 200, 255, 0.1)
 
 
 #FUNCTIONS
+def getActiveWindows():
+    EnumWindows(EnumWindowsProc(foreach_window), 0)
+    for window in activeWindows:
+        if len(window[1]) == 0:
+            activeWindows.remove(window)
+
+def foreach_window(hwnd, lParam):
+    if IsWindowVisible(hwnd):
+        length = GetWindowTextLength(hwnd)
+        buff = ctypes.create_unicode_buffer(length + 1)
+        GetWindowText(hwnd, buff, length + 1)
+        activeWindows.append((hwnd, buff.value))
+    return True
+
+    
 def getMousePos():
     pt = win32gui.GetCursorPos()
     return pt
@@ -56,10 +80,16 @@ def inbound(pos):
     return True
 
 
+def logActiveWindows():
+    log ('\n\nThere are ' + str(len(activeWindows)) + ' windows active:')
+    for window in activeWindows:
+        log (window)
+
+
 def initialize(screen):
     pygame.init()
     screen.fill(COLOR_BACKGROUND)
-    pygame.display.set_caption(title + " - Mouse Capture")
+    pygame.display.set_caption('Mouse Tracer')
     while len(coordinates) < 2:
         pos = getMousePos()
         coordinates.insert(0, pos)
@@ -69,6 +99,13 @@ def generateTitle(title):
     date = str(datetime.now())[:-10].replace(':', '.')
     fileName = 'mouseTrace[' + date + '] - ' + title.strip()
     return fileName
+
+
+def wantedWindowActive():
+    for window in activeWindows:
+        if title in window[1]:
+            return True
+    return False
 
 
 ################# BEGINNING OF THE PROGRAM #################
@@ -82,13 +119,14 @@ log('Line color: ', COLOR_LINE)
 
 #initialize variables
 coordinates = []
-activeWindow = GetWindowText(GetForegroundWindow())
+windowChange = False
+logActiveWindows()
+topWindow = win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
 #wait for program to start...
-   
 print ('\nWaiting for "' + title + '" to start...')
-while title not in activeWindow:
-    activeWindow = GetWindowText(GetForegroundWindow())
+while not title in topWindow:
+    topWindow = win32gui.GetWindowText(win32gui.GetForegroundWindow())
 
 
 #initialize graphics
@@ -98,19 +136,31 @@ initialize(screen)
 
 #start logging mouse movements
 print ('\nGame has started, now logging mouse movements...')
-while title in activeWindow:
+getActiveWindows()
+while wantedWindowActive():
 
     #make sure window keeps responding
     pygame.event.get()
 
-    #Check if we're still in-game
-    activeWindow = GetWindowText(GetForegroundWindow())
-
-    #Log movements
-    pos = getMousePos()
-    if coordinates[0] != pos and inbound(pos):
-        log('Current position: ', pos)
-        coordinates.insert(0, pos)
+    #make sure we're still in the wanted window
+    activeWindows = []
+    getActiveWindows()
+    topWindow = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    if title in topWindow:
+        if windowChange:
+            windowChange = False
+            log('\nWelcome back\n')
+        
+        #Log movements
+        pos = getMousePos()
+        if coordinates[0] != pos and inbound(pos):
+            log ('Current position: ', pos)
+            coordinates.insert(0, pos)
+    else:
+        if not windowChange:
+            log ('\nActive window has changed. Currently active windows:')
+            logActiveWindows()
+            windowChange = True
 
     #Update display
     drawLine(coordinates[0], coordinates[1])
@@ -121,6 +171,7 @@ while title in activeWindow:
 #game is over, save image as vector
 fileName = generateTitle(title)
 print ('\nGame is over!')
+logActiveWindows()
 print ('\nCreating SVG-image...')
 render_svg(fileName, (monitorWidth, monitorHeight), COLOR_BACKGROUND, COLOR_LINE, coordinates)
 render_svg(fileName + ' (fullRender)', (monitorWidth, monitorHeight), (0, 0, 0), (255, 255, 255, 1), coordinates)
--- svgCreator.py
+++ svgCreator.py
@@ -6,15 +6,13 @@ def render_svg(name, size, color_background, color_line, coordinates):
     #start writing file
     image = open(name + '.svg', 'w')
     writePreamble(image, size, color_background)
-    coordsOld = coordinates.pop(0)
+    image.write('<polygon points="')
     for coords in coordinates:
-        x1 = str(coordsOld[0])
-        y1 = str(coordsOld[1])
-        x2 = str(coords[0])
-        y2 = str(coords[1])
-        image.write('<line x1="' + x1 + '" y1="' + y1 + '" x2="' + x2 + '" y2="' + y2 + '" ')
-        image.write('style="stroke:rgba' + str(color_line) + ';stroke-width:2" />\n')
+        x = str(coords[0])
+        y = str(coords[1])
+        image.write(x + ',' + y + ' ')
         coordsOld = coords
+    image.write('" style="stroke:rgba' + str(color_line) + ';stroke-width:1" />')
     writePostamble(image)
 
 
@@ -28,7 +26,7 @@ def writePreamble(image, size, color_background):
     image.write('<svg width="' +  width + '" height="' + height + '" ')
     image.write('version="1.1" xmlns="http://www.w3.org/2000/svg">\n')
     image.write('<rect width="' + width + '" height="' + height + '" ')
-    image.write('style="fill:rgb' + str(color_background) +'" />')
+    image.write('style="fill:rgb' + str(color_background) +'" />\n')
 
 
 
